124a19ccca262a50a5d59d06728ef5038acf61d6
jmu35764 <jmu35764@email.vccs.edu>
2026-04-02 18:53:36 -0400

    Wrote up function for getNumber. Currently have an error. Will save & close and see if the error remains, becuase it doesn't seem to make sense.

--- Number Array Class Program/NumberArray.cpp
+++ Number Array Class Program/NumberArray.cpp
@@ -15,3 +15,34 @@ NumberArray::NumberArray(int size = MAX_SIZE)
 	}
 
 }
+
+/*/NumberArray::~NumberArray();
+{
+	delete data[];
+}*/
+
+void NumberArray::setNumber(int index, double value)
+{
+	if (index >= 0 && index < a_size)
+	{
+		data[index] = value;
+	}
+
+	else
+	{
+		cout << "Invalid Index Input" << endl;
+	}
+}
+
+double NumberArray::getNumber(int index)
+{
+	if (index >= 0 && index < a_size)
+	{
+		return data[index];
+	}
+
+	else
+	{
+		cout << "Placeholder for static default value" << endl;
+	}
+}
