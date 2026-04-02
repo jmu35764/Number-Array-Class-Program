67c65ed0c011a391d067c5b5006f7f0721d25343
jmu35764 <jmu35764@email.vccs.edu>
2026-04-02 18:20:00 -0400

    Add project files.

---
+++ Number Array Class Program/NumberArray.cpp
@@ -0,0 +1,17 @@
+#include "NumberArray.h"
+#include <iostream>
+
+using namespace std;
+
+NumberArray::NumberArray(int size = MAX_SIZE)
+{
+	a_size = size;
+	
+	data = new double[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		
+	}
+
+}
---
+++ Number Array Class Program/NumberArray.h
@@ -0,0 +1,31 @@
+
+#ifndef NUMARRAY_H
+#define NUMARRAY_H
+
+class NumberArray
+{
+private:
+
+	int a_size;
+	double* data;
+	static const int MAX_SIZE = 12;
+
+public:
+	NumberArray(int size = MAX_SIZE);
+
+	~NumberArray();
+
+	void setNumber(int index, double value);
+
+	double getNumber(int index) const;
+
+	double getMin() const;
+
+	double getMax() const;
+
+	double getAverage() const;
+
+
+};
+
+#endif // !NUMARRAY_h
